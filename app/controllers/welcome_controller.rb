3d399bf1a3e3c7a638e8b634b6c0f69741570ffc
ajbraus <ajbraus@gmail.com>
2013-05-01 10:03:32 -0500

    fixed pie chart a bit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,33 +4,37 @@ class WelcomeController < ApplicationController
   	@subscription_counter = EmailSubscription.all.count
   	
   	@chart = LazyHighCharts::HighChart.new('pie') do |f|
-      f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+      f.chart({:defaultSeriesType=>"pie"}) #, :margin=> [50, 200, 60, 170]} )
       series = {
                :type=> 'pie',
                :name=> 'Personal Price Index',
                :data=> [
-                  ['Food',   45.0],
-                  ['Apparel',       26.8],
-                  {
-                     :name=> 'Restaurants',    
-                     :y=> 12.8,
-                     :sliced=> true,
-                     :selected=> true
-                  },
-                  ['Transportation (w/o car)',    8.5],
-                  ['Transportation (w/ car)',     6.2],
-                  ['Fuel',   0.7]
+                  ['Apparel',   15.0],
+                  ['Groceries',       26.8],
+                  ['Technology',     8.2],
+                  ['Medical',     8.2],
+                  ['Education',     8.2],
+                  ['Personal Care',     8.2],
+                  ['Recreation',     8.2],
+                  ['Housing',     8.2],
+                  ['Transportation',     6.2]
+                  # {
+                  #    :name=> 'Restaurants',    
+                  #    :y=> 12.8,
+                  #    :sliced=> true,
+                  #    :selected=> true
+                  # }
                ]
       }
       f.series(series)
-      f.options[:title][:text] = "Your Personal Price Index"
-      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      #f.options[:title][:text] = "Your Personal Price Index"
+      #f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
       f.plot_options(:pie=>{
         :allowPointSelect=>true, 
-        :cursor=>"pointer" , 
+        :cursor=>"pointer", 
         :dataLabels=>{
           :enabled=>true,
-          :color=>"white",
+          :color=>"black",
           :style=>{
             :font=>"13px Trebuchet MS, Verdana, sans-serif"
           }
